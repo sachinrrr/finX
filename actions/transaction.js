7b5f2a4e6eaa929bb9307eea4ebd95a83704f361
sachinrrr <sachinrubera1@gmail.com>
2025-12-22 15:05:01 +0530

    deploy

--- actions/transaction.js
+++ actions/transaction.js
@@ -1,12 +1,10 @@
 "use server";
 
-import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { Buffer } from "buffer";
-import aj from "@/lib/arcjet";
-import { request } from "@arcjet/next";
 
 const serializeAmount = (obj) => ({
   ...obj,
@@ -129,32 +127,6 @@ export async function createTransaction(data) {
   try {
     const { userId } = await auth();
     if (!userId) throw new Error("Unauthorized");
-
-    // Get request data for ArcJet
-    const req = await request();
-
-    // Check rate limit
-    const decision = await aj.protect(req, {
-      userId,
-      requested: 1, // Specify how many tokens to consume
-    });
-
-    if (decision.isDenied()) {
-      if (decision.reason.isRateLimit()) {
-        const { remaining, reset } = decision.reason;
-        console.error({
-          code: "RATE_LIMIT_EXCEEDED",
-          details: {
-            remaining,
-            resetInSeconds: reset,
-          },
-        });
-
-        throw new Error("Too many requests. Please try again later.");
-      }
-
-      throw new Error("Request blocked");
-    }
 
     const user = await db.user.findUnique({
       where: { clerkUserId: userId },
