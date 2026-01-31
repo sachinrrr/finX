7bcb790f2db8b5fd5a3a4a52d0e65a1104ef7c48
sachinrrr <sachinrubera1@gmail.com>
2026-01-31 23:00:39 +0530

    deploy

--- components/ui/progress.jsx
+++ components/ui/progress.jsx
@@ -10,13 +10,13 @@ const Progress = React.forwardRef(
     <ProgressPrimitive.Root
       ref={ref}
       className={cn(
-        "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
+        "relative h-2 w-full overflow-hidden rounded-full bg-green-200",
         className
       )}
       {...props}
     >
       <ProgressPrimitive.Indicator
-        className={`h-full w-full flex-1 bg-primary transition-all ${extraStyles}`}
+        className={`h-full w-full flex-1 bg-green-500 transition-all ${extraStyles}`}
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
